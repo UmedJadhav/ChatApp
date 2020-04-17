8f2546aa9f7182e7681e0e9276f05dff5cfa5a8b
Umed Jadhav <umed@browserstack.com>
2020-04-17 18:20:52 +0530

    Created basic socket.io setup with connect and disconnect event listeners

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 
 const PORT = process.env.PORT || 3000 ;
 const public_path = path.join(__dirname, '/../public')
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 
 app.use(express.static(public_path));
-app.listen(PORT, ()=>{
+
+io.on('connection', (socket)=>{
+  console.log(`A new user just connected`);
+
+  socket.on('disconnect', ()=>{
+    console.log(`user just disconnected`);
+  })
+})
+
+
+server.listen(PORT, ()=>{
   console.log(`Server is up at port ${PORT}`)});
